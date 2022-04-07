85e8667d6d612ff9f21cfbb6ae43d18b5cd3d9f9
Josefinamf <jmartinezfrugoni@udesa.edu.ar>
2022-04-07 10:04:37 -0300

    carpeta db, con index.js, string de libros (faltacompletar)

---
+++ ProyectoIntegrador-Programacion_II/db/index.js
@@ -0,0 +1,170 @@
+const libros ={
+    lista: [{
+        title:'Harry Potter y el cáliz de fuego',
+        autor:'JK Rowling',
+        genre:'Fantastico',
+        edition: 2018,
+        editorial:'Caligrama',
+        language:'Español'
+    }
+    {
+        title:'El señor de los anillos: El retorno del rey',
+        autor: '',
+        genre:'Fantastico',
+        edition: 2017,
+        editorial:'Lulu',
+        language:'Ingles'
+    }
+    {
+        title:'El señor de los anillos: Las dos torres',
+        autor: '',
+        genre:'Fantastico',
+        edition: 2017,
+        editorial:'Lulu',
+        language:'Ingles'
+    }
+    {
+        title:'Eragon',
+        autor: '',
+        genre:'Fantastico',
+        edition: 2018,
+        editorial:'Caligrama',
+        language:'Español'
+    }
+    {
+        title:'Las cronicas de Narnia; el leon, la bruja y el ropero',
+        autor: '',
+        genre:'Fantastico',
+        edition: 2019,
+        editorial:'Círculo Rojo',
+        language:'Inlges'
+    }
+    {
+        title:'Harry Potter y la Piedra Filosofal',
+        autor:'',
+        genre:'Fantastico',
+        edition: 2018,
+        editorial:'Caligrama',
+        language:'Español'
+    }
+    {
+        title:'Harry Potter y la Orden del Fénix',
+        autor:'',
+        genre:'Fantastico',
+        edition: 2017,
+        editorial:'Caligrama',
+        language:'Español'
+    }
+    {
+        title:'Harry Potter y las Reliquias de la Muerte',
+        autor:'',
+        genre:'Fantastico',
+        edition: 2018,
+        editorial:'Caligrama',
+        language:'Español'
+    }
+    {
+        title:'Harry Potter y el prisionero de Azkaban',
+        autor:'',
+        genre:'Fantastico',
+        edition: 2019,
+        editorial:'Caligrama',
+        language:'Español'
+    }
+    {
+        title:'Harry Potter y el misterio del principe',
+        autor:'',
+        genre:'Fantastico',
+        edition: 2018,
+        editorial:'Caligrama',
+        language:'Español'
+    }
+    {
+        title:'Harry Potter y la cámara secreta',
+        autor:'',
+        genre:'Fantastico',
+        edition: 2017,
+        editorial:'Caligrama',
+        language:'Español'
+    }
+    {
+        title:'Bajo la misma estrella',
+        autor:'John Green',
+        genre:'Romance',
+        edition: 2016,
+        editorial:'Bubok',
+        language:'Español'
+    }
+    {
+        title:'Frigid',
+        autor:'Jennifer L Armentrout',
+        genre:'Romance',
+        edition: 2021,
+        editorial:'Bubok',
+        language:'Ingles'
+    }
+    {
+        title:'Ciudad de papel',
+        autor:'John Green',
+        genre:'Romance',
+        edition: 2016,
+        editorial:'Círculo Rojo',
+        language:'Español'
+    }
+    {
+        title:'It ends with us',
+        autor:'Colleen Hoover',
+        genre:'Romance',
+        edition: 2021,
+        editorial:'Bubok',
+        language:'Ingles'
+    }
+    {
+        title:'The Proposal',
+        autor:'Jasmine Guillory',
+        genre:'Romance',
+        edition: 2017,
+        editorial:'Bubok',
+        language:'Ingles'
+    }
+    {
+        title:'Rojo, blanco y sangre azul',
+        autor:'Cassey Mcquinston',
+        genre:'Romance',
+        edition: 2019,
+        editorial:'Círculo Rojo',
+        language:'Español'
+    }
+    {
+        title:'Cariño, cuánto te odio',
+        autor:'Sally Thorne',
+        genre:'Romance',
+        edition: 2016,
+        editorial:'Caligrama',
+        language:'Español'
+    }
+    {
+        title:'People We Meet on Vacation',
+        autor:'Emily Henry',
+        genre:'Romance',
+        edition: 2021,
+        editorial:'Bubok',
+        language:'Ingles'
+    }
+    {
+        title:'El cuaderno de Noah',
+        autor:'Nicholas Sparks',
+        genre:'Romance',
+        edition: 2018,
+        editorial:'Bubok',
+        language:'Español'
+    }
+    {
+
+    }
+    {
+
+    }
+]
+}
+module.exports = libros;
