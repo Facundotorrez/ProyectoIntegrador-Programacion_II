c825524c3c0407b267d22ef934935b91adbc7635
Josefinamf <jmartinezfrugoni@udesa.edu.ar>
2022-06-22 15:29:46 -0300

    update productos, editar producto, eliminar producto, arreglo del controlador usuarios, crear comentarios(hay que fijarsesi salio bien), comente show dentro de controladores xq me corre con un error

--- routes/products.js
+++ routes/products.js
@@ -21,15 +21,21 @@ router.get('/index', productsController.index);
 
 router.get('/product-add', productsController.productAdd);
 
-router.post('/store', upload.single('foto'), productsController.store); //ruta que guarda datos.
+router.post('/store', upload.single('foto'), productsController.store); //ruta que guarda datos del form.
 
-//router.post('/comentarios', productsController.storeComentarios); //comments
+router.post('/comentarios', productsController.storeComentarios); //comments
 
-router.get('/id', productsController.show); //va a mostrar el form de carga.
+router.get('/:id', productsController.show); //va a mostrar el form de carga.
 
-router.get('edit/:id', productsController.editar);
+router.get('/edit/:id', productsController.editar);
 
-//router.post('/update/:id', uplad.single('foto'), productsController.update); // completar en el controlador
+//en esta parte seria similar a un agregar producto, la estructura es similar a la de editar perdil cuando uno se registra, ya que, hay que establecer denuevo los campos y poner las notificaciones de que no pueden haber campos vacios,etc.
+
+router.post('/edit/:id', upload.single('foto'), productsController.update); // completar en el controlador
+
+//agregar ruta de eliminar producto
+
+router.post('/eliminar/:id', productsController.eliminar);
 
 
 
